d794e4af52209631207eb0eb5e0507b32108119f
Amaury Bouchard <amaury@amaury.net>
2018-02-06 14:35:24 +0100

    ConfigObjectGenerator: Debugs and improvements.

--- bin/configObjectGenerator.php
+++ bin/configObjectGenerator.php
@@ -5,10 +5,9 @@
  * Programme de génération du code de configuration, à partir d'un fichier temma.json.
  *
  * @author	Amaury Bouchard <amaury.bouchard@finemedia.fr>
- * @copyright	© 2012, Fine Media
+ * @copyright	© 2012-2018
  * @package	Temma
  * @subpackage	bin
- * @version	$Id$
  */
 
 require_once('finebase/FineAutoload.php');
@@ -48,8 +47,13 @@ else
 	$viewsPath = null;
 if (isset($ini['includePaths']) && is_array($ini['includePaths']))
 	$pathsToInclude = array_merge($pathsToInclude, $ini['includePaths']);
-if (!empty($pathsToInclude))
-	$pathsToInclude = implode(PATH_SEPARATOR, $pathsToInclude);
+$pathsToInclude = empty($pathsToInclude) ? null : implode(PATH_SEPARATOR, $pathsToInclude);
+// extra configuration
+$xtra = array();
+foreach ($ini as $key => $value) {
+	if (substr($key, 0, strlen(\Temma\Config::XTRA_CONFIG_PREFIX)) === \Temma\Config::XTRA_CONFIG_PREFIX)
+		$xtra[$key] = $value;
+}
 
 /* Constitution du code de génération. */
 $s = '<' . '?php
@@ -59,18 +63,21 @@ namespace Temma {
 ';
 
 if (isset($ini['application']['dataSources']) && is_array($ini['application']['dataSources'])) {
-	foreach ($ini['application']['dataSources'] as $name => $dsn)
+	// write data sources private variables
+	foreach ($ini['application']['dataSources'] as $name => $dsn) {
 		$s .= '		public $' . $name . ' = null;' . "\n";
+	}
+	$s .= "\n";
 }
-$s .= '
-		final public function __construct($dataSources, \FineSession $session=null, \Temma\Config $config, \Temma\Request $request=null, $executor=null) {
+$s .= '		final public function __construct($dataSources, \FineSession $session=null, \Temma\Config $config, \Temma\Request $request=null, $executor=null) {
 ';
 if (isset($ini['application']['dataSources']) && is_array($ini['application']['dataSources'])) {
-	foreach ($ini['application']['dataSources'] as $name => $dsn) 
-		$s .= '			$this->' . $name . ' = $dataSources["' . $name . '"];' . "\n";
+	foreach ($ini['application']['dataSources'] as $name => $dsn) {
+		$s .= '			$this->' . $name . ' = $dataSources[\'' . $name . '\'];' . "\n";
+	}
+	$s .= "\n";
 }
-$s .= '
-			parent::__construct($dataSources, $session, $config, $request, $executor);
+$s .= '			parent::__construct($dataSources, $session, $config, $request, $executor);
 		}
 	}
 }
@@ -83,69 +90,62 @@ namespace {
 			// paths
 ';
 if ($controllersPath)
-	$s .= "\t\t\t" . '$this->_controllersPath = "' . $controllersPath . '";' . "\n";
+	$s .= "\t\t\t" . '$this->_controllersPath = \'' . $controllersPath . "';\n";
 if ($viewsPath)
-	$s .= "\t\t\t" . '$this->_viewsPath = "' . $viewsPath . '";' . "\n";
+	$s .= "\t\t\t" . '$this->_viewsPath = \'' . $viewsPath . "';\n";
 if ($includesPath)
-	$s .= "\t\t\t" . '$this->_includesPath = "' . $includesPath . '";' . "\n";
+	$s .= "\t\t\t" . '$this->_includesPath = \'' . $includesPath . "';\n";
 if (!empty($pathsToInclude))
-	$s .= "\t\t\t" . '\FineAutoload::addIncludePath("' . $pathsToInclude . '");' . "\n";
-$s .= '			$this->_templatesPath = "' . $templatesPath . '";
-			$this->_webPath = "' . $webPath . '";
-			$this->_tmpPath = "' . $tmpPath . '";
-			$this->_logPath = "' . $logPath . '";
+	$s .= "\t\t\t" . '\FineAutoload::addIncludePath(\'' . $pathsToInclude . "');\n";
+$s .= '			$this->_templatesPath = \'' . $templatesPath . '\';
+			$this->_webPath = \'' . $webPath . '\';
+			$this->_tmpPath = \'' . $tmpPath . '\';
+			$this->_logPath = \'' . $logPath . '\';
 			// log
-			\FineLog::setLogFile("' . $logFile . '");
-			\FineLog::setThreshold(' . ((isset($ini['loglevels']) && is_array($ini['loglevels'])) ?
-						    ('unserialize("' . str_replace('"', '\"', serialize($ini['loglevels'])) . '")') :
-						    'self::LOG_LEVEL') . ');
-
+			\FineLog::setLogFile(\'' . $logFile . '\');
+			\FineLog::setThreshold(' . ((isset($ini['loglevels']) && is_array($ini['loglevels'])) ? var_export($ini['loglevels'], true) : 'self::LOG_LEVEL') . ');
 			// error pages
-			$this->_errorPages = ' . ((isset($ini['errorPages']) && is_array($ini['errorPages'])) ?
-						  ('unserialize("' . str_replace('"', '\"', serialize($ini['errorPages'])) . '")') :
-						  'array()') . ';
-
-			// data sources
-			$this->_dataSources = array(';
+			$this->_errorPages = ' . ((isset($ini['errorPages']) && is_array($ini['errorPages'])) ? var_export($ini['errorPages'], true) : 'null') . ';
+';
 if (isset($ini['application']['dataSources']) && is_array($ini['application']['dataSources'])) {
-	foreach ($ini['application']['dataSources'] as $name => $dsn)
+	$s .= '			// data sources
+			$this->_dataSources = array(';
+	foreach ($ini['application']['dataSources'] as $name => $dsn) {
 		$s .= "\n\t\t\t\t'$name' => FineDatasource::factory('$dsn'),";
+	}
+	$s .= "\n			);\n";
 }
-$s .= '
-			);
-			// routes
-			$this->_routes = ' . ((isset($ini['routes']) && is_array($ini['routes'])) ?
-					      ('unserialize("' . str_replace('"', '\"', serialize($ini['routes'])) . '")') :
-					      'array()') . ';
+$s .= '			// routes
+			$this->_routes = ' . ((isset($ini['routes']) && is_array($ini['routes'])) ? var_export($ini['routes'], true) : 'null') . ';
 			// plugins
-			$this->_plugins = ' . ((isset($ini['plugins']) && is_array($ini['plugins'])) ?
-					       ('unserialize("' . str_replace('"', '\"', serialize($ini['plugins'])) . '")') :
-					       'array()') . ';
-			// extended configuration
+			$this->_plugins = ' . ((isset($ini['plugins']) && is_array($ini['plugins'])) ? var_export($init['plugins'], true) : 'null') . ';
+';
+if (!empty($xtra)) {
+	$s .= '			// extended configuration
 			$this->_extraConfig = array(';
-foreach ($ini as $key => $value) {
-	if (substr($key, 0, strlen(\Temma\Config::XTRA_CONFIG_PREFIX)) === \Temma\Config::XTRA_CONFIG_PREFIX)
-		$s .= "\n\t\t\t\t'$key' => unserialize(\"" . str_replace('"', '\"', serialize($value)) . "\"),";
+	foreach ($xtra as $key => $value) {
+		$s .= "\n\t\t\t\t'$key' => " . var_export($value, true) . ",";
+	}
+	$s .= "\n			);\n";
 }
-$s .= '			);
-			// autoimport
-			$this->_autoimport = ' . (isset($ini['autoimport']) ?
-						  ('unserialize("' . str_replace('"', '\"', serialize($ini['autoimport'])) . '")') :
-						  'null') . ';
+$s .= '			// autoimport
+			$this->_autoimport = ' . (isset($ini['autoimport']) ? var_export($ini['autoimport'], true) : 'null') . ';
 			// session management
 			$this->_enableSessions = ' . ((isset($ini['application']['enableSessions']) && $ini['application']['enableSessions'] === false) ?
 						      'false' : 'true') . ';
-			$this->_sessionName = "' . ((isset($ini['application']['sessionName']) && !empty($ini['application']['sessionName'])) ?
-						    $ini['application']['sessionName'] : self::SESSION_NAME) . '";
+			$this->_sessionName = ' . ((isset($ini['application']['sessionName']) && !empty($ini['application']['sessionName'])) ?
+						    ("'" . $ini['application']['sessionName'] . "'") : 'self::SESSION_NAME') . ';
 			$this->_sessionSource = ' . ((isset($ini['application']['sessionSource']) && !empty($ini['application']['sessionSource'])) ?
-						     ('"' . $ini['application']['sessionSource'] . '"') : 'null') . ';
+						     ("'" . $ini['application']['sessionSource'] . "'") : 'null') . ';
 			// controllers
 			$this->_rootController = ' . (isset($ini['application']['rootController']) ?
-						      ('"' . $ini['application']['rootController'] . '"') : 'null') . ';
+						      ("'" . $ini['application']['rootController'] . "'") : 'null') . ';
 			$this->_defaultController = ' . (isset($ini['application']['defaultController']) ?
-							 ('"' . $ini['application']['defaultController'] . '"') : 'null') . ';
+							 ("'" . $ini['application']['defaultController'] . "'") : 'null') . ';
 			$this->_proxyController = ' . (isset($ini['application']['proxyController']) ?
-						       ('"' . $ini['application']['proxyController'] . '"') : 'null') . ';
+						       ("'" . $ini['application']['proxyController'] . "'") : 'null') . ';
+			$this->_defaultNamespace = ' . (isset($ini['application']['defaultNamespace']) ?
+							("'" . $ini['application']['defaultNamespace'] . "'") : 'null') . ';
 			// create a new executorController
 			$this->_executorController = new \Temma\Controller($this->_dataSources, null, $this, null, null);
 			// add this configuration object to Temma framework
@@ -161,7 +161,7 @@ file_put_contents($destination, $s);
 /** Affiche la documentation du programme et quitte. */
 function usage() {
 	print("Usage:\n");
-	print("	" . $_SERVER['argv'][0] . " project_root_dir\n");
+	print('	' . $_SERVER['argv'][0] . " project_root_dir\n");
 	exit(1);
 }
 
