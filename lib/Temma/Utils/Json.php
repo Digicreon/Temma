594cb045183f656ae83596722297ce0157b98365
Amaury Bouchard <amaury@amaury.net>
2023-03-01 15:21:26 +0100

    Add \Temma\Utils\Json::decodeFile() method.

--- lib/Temma/Utils/Json.php
+++ lib/Temma/Utils/Json.php
@@ -22,9 +22,18 @@ class Json {
 	 * @return	mixed	The decoded data.
 	 * @link	https://stackoverflow.com/questions/8148797/a-json-parser-for-php-that-supports-comments/43439966#43439966
 	 */
-	static public function decode(string $json, bool $assoc=false, int $depth=512, $options = 0) /* : mixed */ {
+	static public function decode(string $json, bool $assoc=false, int $depth=512, $options=0) /* : mixed */ {
 		$json = preg_replace('~(" (?:[^"\\\\] | \\\\\\\\ | \\\\")*+ ") | \# [^\v]*+ | // [^\v]*+ | /\* .*? \*/~xs', '$1', $json);
 		return (json_decode($json, $assoc, $depth, $options));
+	}
+	/**
+	 * Decode a JSON stream stored in a file. If the JSON stream contains comments, they are discarded.
+	 * The JSON data is returned as an associative array, not as an object.
+	 * @param	string	$path	Path to the file.
+	 * @return	mixed	The decoded data.
+	 */
+	static public function decodeFile(string $path) /* : mixed */ {
+		return (self::decode(file_get_contents($path), true));
 	}
 }
 
