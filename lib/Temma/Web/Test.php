fa673cfe00f6ce2d6248095eaeb4a30ff24b3326
Amaury Bouchard <amaury@amaury.net>
2023-12-25 21:24:34 +0100

    Web\Test: general improvement, management of cookies.

--- lib/Temma/Web/Test.php
+++ lib/Temma/Web/Test.php
@@ -19,7 +19,9 @@ class Test {
 	protected ?string $_appPath = null;
 	/** Path to the configuration file. */
 	protected ?string $_jsonConfigPath = null;
-	/** Loader. */
+	/** Original loader, given at creation. */
+	protected ?TµLoader $_originalLoader = null;
+	/** Loader used for the last request. */
 	protected ?TµLoader $_loader = null;
 
 	/**
@@ -32,23 +34,39 @@ class Test {
 	public function __construct(?string $appPath=null, ?string $jsonConfigPath=null, ?TµLoader $loader=null) {
 		$this->_appPath = $appPath;
 		$this->_jsonConfigPath = $jsonConfigPath;
-		$this->_loader = $loader;
+		$this->_originalLoader = $loader;
 	}
 	/**
 	 * Returns the loader object.
-	 * @return	TµLoader	The created loader object.
+	 * @return	?TµLoader	The created loader object. Null if no request has been done yet.
 	 */
-	public function getLoader() : TµLoader {
+	public function getLoader() : ?TµLoader {
+		return ($this->_loader);
+	}
+	/**
+	 * Executes a request and returns the loader object.
+	 * @param	string	$url		URL to execute (starting with a slash character).
+	 * @param	string	$httpMethod	(optional) HTTP method ("GET", "POST"...). Defaults to "GET".
+	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
+	 * @param	?array	$cookies	(optional) Associative array of cookies. Defaults to null.
+	 * @return	\Temma\Base\Loader	The dependency injection component.
+	 */
+	public function execLoader(string $url, string $httpMethod='GET', ?array $data=null, ?array $cookies=null) : \Temma\Base\Loader {
+		$temma = $this->_createEnvironment($url, $data, $httpMethod);
+		ob_start();
+		$temma->process(processView: null, sendHeaders: false);
+		ob_end_clean();
 		return ($this->_loader);
 	}
 	/**
 	 * Executes a request and returns the response object.
 	 * @param	string	$url		URL to execute (starting with a slash character).
-	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
 	 * @param	string	$httpMethod	(optional) HTTP method ("GET", "POST"...). Defaults to "GET".
+	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
+	 * @param	?array	$cookies	(optional) Associative array of cookies. Defaults to null.
 	 * @return	\Temma\Web\Response	The response object.
 	 */
-	public function exec(string $url, ?array $data=null, string $httpMethod='GET') : \Temma\Web\Response {
+	public function execResponse(string $url, string $httpMethod='GET', ?array $data=null, ?array $cookies=null) : \Temma\Web\Response {
 		$temma = $this->_createEnvironment($url, $data, $httpMethod);
 		ob_start();
 		$temma->process(processView: null, sendHeaders: false);
@@ -58,11 +76,12 @@ class Test {
 	/**
 	 * Executes a request and returns the defined variables.
 	 * @param	string	$url		URL to execute (starting with a slash character).
-	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
 	 * @param	string	$httpMethod	(optional) HTTP method ("GET", "POST"...). Defaults to "GET".
+	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
+	 * @param	?array	$cookies	(optional) Associative array of cookies. Defaults to null.
 	 * @return	?string|array	Redirection URL or associative array of template variables.
 	 */
-	public function execData(string $url, ?array $data=null, string $httpMethod='GET') : null|string|array {
+	public function execData(string $url, string $httpMethod='GET', ?array $data=null, ?array $cookies=null) : null|string|array {
 		$temma = $this->_createEnvironment($url, $data, $httpMethod);
 		ob_start();
 		$result = $temma->process(processView: false, sendHeaders: false);
@@ -72,11 +91,12 @@ class Test {
 	/**
 	 * Executes a request and returns its output stream.
 	 * @param	string	$url		URL to execute (starting with a slash character).
-	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
 	 * @param	string	$httpMethod	(optional) HTTP method ("GET", "POST"...). Defaults to "GET".
+	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
+	 * @param	?array	$cookies	(optional) Associative array of cookies. Defaults to null.
 	 * @return	string	The output (depending on the view, HTML by default).
 	 */
-	public function execOutput(string $url, ?array $data=null, string $httpMethod='GET') : string {
+	public function execOutput(string $url, string $httpMethod='GET', ?array $data=null, ?array $cookies=null) : string {
 		$temma = $this->_createEnvironment($url, $data, $httpMethod);
 		ob_start();
 		$temma->process(sendHeaders: false);
@@ -88,26 +108,29 @@ class Test {
 	/**
 	 * Creates the execution environment.
 	 * @param	string	$url		URL to execute (starting with a slash character).
-	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
 	 * @param	string	$httpMethod	(optional) HTTP method ("GET", "POST"...). Defaults to "GET".
+	 * @param	?array	$data		(optional) Associative array of parameters (GET or POST parameters, depending on the $httpMethod parameter). Defaults to null.
+	 * @param	?array	$cookies	(optional) Associative array of cookies. Defaults to null.
 	 * @return	\Temma\Web\Framework	An instance of the framework main object.
 	 */
-	protected function _createEnvironment(string $url, ?array $data=null, string $httpMethod='GET') : \Temma\Web\Framework {
+	protected function _createEnvironment(string $url, string $httpMethod='GET', ?array $data=null, ?array $cookies=null) : \Temma\Web\Framework {
+		// reset
+		$_GET = $_POST = [];
+		$_COOKIE = $cookies ?? [];
+		$_SERVER['REQUEST_METHOD'] = $httpMethod;
+		$_SERVER['REQUEST_URI'] = $url;
 		// loader creation
-		if ($this->_loader)
-			$loader = clone $this->_loader;
+		if ($this->_originalLoader)
+			$this->_loader = clone $this->_originalLoader;
 		else
-			$loader = new TµLoader();
+			$this->_loader = new TµLoader();
 		// configuration loading
 		if ($this->_appPath) {
 			$config = new \Temma\Web\Config($this->_appPath);
 			$config->readConfigurationFile($this->_jsonConfigPath);
-			$loader->config = $config;
+			$this->_loader->config = $config;
 		}
-		// method and data
-		$_SERVER['REQUEST_METHOD'] = $httpMethod;
-		$_SERVER['REQUEST_URI'] = $url;
-		$_GET = $_POST = [];
+		// data
 		if ($data) {
 			if ($httpMethod == 'GET')
 				$_GET = $data;
@@ -116,9 +139,9 @@ class Test {
 			$_REQUEST = array_merge($_POST, $_GET, $_COOKIE);
 		}
 		// request
-		$loader->request = new \Temma\Web\Request($url);
+		$this->_loader->request = new \Temma\Web\Request($url);
 		// framework
-		$temma = new \Temma\Web\Framework($loader);
+		$temma = new \Temma\Web\Framework($this->_loader);
 		return ($temma);
 	}
 }
