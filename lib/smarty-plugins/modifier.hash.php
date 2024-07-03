840bcd3ebe29673d60476d982e631e370b0fcc7c
Amaury Bouchard <amaury@amaury.net>
2024-07-03 22:25:48 +0200

    Add 'hash' Smarty modifier.

---
+++ lib/smarty-plugins/modifier.hash.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Smarty modifier to hash data.
+ * @param	?string	$content	Content to hash.
+ * @param	string	$algo		(optional) Name of the used algorithm. Defaults to SHA-256.
+ * @return	string	The hashed string.
+ * @author	Amaury Bouchard <amaury@æmaury.net>
+ * @copyright	© 2024, Amaury Bouchard
+ */
+function smarty_modifier_hash(?string $content, string $algo='sha256') {
+	return hash($algo, ($content ?? ''));
+}
+
