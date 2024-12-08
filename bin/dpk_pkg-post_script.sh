e940d822c1f71cfda0accb7f9875f6be938234ce
Amaury Bouchard <amaury@amaury.net>
2024-12-08 17:58:17 +0100

    Update post-packaging script.

--- bin/dpk_pkg-post_script.sh
+++ bin/dpk_pkg-post_script.sh
@@ -19,81 +19,87 @@ fi
 # check if the directory exists
 if [ ! -d /opt/temma-lib ]; then
 	echo "No directory '/opt/temma-lib' found."
-	exit 2
+else
+	echo "== Update 'temma-lib'"
+	# copy files
+	pushd /opt/Temma > /dev/null
+	cp -a lib/*         /opt/temma-lib/lib/
+	popd > /dev/null
+	# commit tiles
+	pushd /opt/temma-lib > /dev/null
+	if [[ -n $(git status --porcelain) ]]; then
+		git add .
+		git commit -m "Update from Temma main repository version '$PKG_TAG'."
+		git push
+		dpk pkg --tag=$PKG_TAG
+	fi
+	echo "   done"
+	popd > /dev/null
 fi
-echo "== Update 'temma-lib'"
-# copy files
-pushd /opt/Temma > /dev/null
-cp -a lib/*         /opt/temma-lib/lib/
-popd > /dev/null
-# commit tiles
-pushd /opt/temma-lib > /dev/null
-if [[ -n $(git status --porcelain) ]]; then
-	git add .
-	git commit -m "Update from Temma main repository version '$PKG_TAG'."
-	git push
-	dpk pkg --tag=$PKG_TAG
-fi
-echo "   done"
-popd > /dev/null
 
 # ########## temma-skel-web ##########
 # check if the directory exists
 if [ ! -d /opt/temma-skel-web ]; then
 	echo "No directory '/opt/temma-skel-web' found."
-	exit 3
+else
+	echo "== Update 'temma-skel-web'"
+	# copy files
+	pushd /opt/Temma > /dev/null
+	cp    bin/comma          /opt/temma-skel-web/bin/
+	cp    etc/apache.conf    /opt/temma-skel-web/etc/
+	cp    etc/temma-mini.php /opt/temma-skel-web/etc/temma.php
+	cp    etc/temma-full.php /opt/temma-skel-web/etc/
+	cp -a etc/asynk          /opt/temma-skel-web/etc/
+	cp -a templates/*        /opt/temma-skel-web/templates/
+	cp    tests/*            /opt/temma-skel-web/tests/
+	cp    www/index.php      /opt/temma-skel-web/www/
+	cp    www/*.html         /opt/temma-skel-web/www/
+	cp    www/.htaccess      /opt/temma-skel-web/www/
+	cp    .htaccess          /opt/temma-skel-web/
+	# manage composer.json
+	pushd /opt/temma-skel-web > /dev/null
+	sed "s/\"digicreon\/temma-lib\": \"[^\"]*\"/\"digicreon\/temma-lib\": \"$PKG_TAG\"/" composer.json > composer.json.tmp
+	mv composer.json.tmp composer.json
+	# commit tiles
+	if [[ -n $(git status --porcelain) ]]; then
+		git add .
+		git commit -m "Update from Temma main repository version '$PKG_TAG'."
+		git push
+		dpk pkg --tag=$PKG_TAG
+	fi
+	echo "   done"
+	popd > /dev/null
 fi
-echo "== Update 'temma-skel-web'"
-# copy files
-pushd /opt/Temma > /dev/null
-cp    bin/comma          /opt/temma-skel-web/bin/
-cp    etc/apache.conf    /opt/temma-skel-web/etc/
-cp    etc/temma-mini.php /opt/temma-skel-web/etc/temma.php
-cp    etc/temma-full.php /opt/temma-skel-web/etc/
-cp -a etc/asynk          /opt/temma-skel-web/etc/
-cp -a templates/*        /opt/temma-skel-web/templates/
-cp    tests/*            /opt/temma-skel-web/tests/
-cp    www/index.php      /opt/temma-skel-web/www/
-cp    www/*.html         /opt/temma-skel-web/www/
-cp    www/.htaccess      /opt/temma-skel-web/www/
-cp    .htaccess          /opt/temma-skel-web/
-# commit tiles
-pushd /opt/temma-skel-web > /dev/null
-if [[ -n $(git status --porcelain) ]]; then
-	git add .
-	git commit -m "Update from Temma main repository version '$PKG_TAG'."
-	git push
-	dpk pkg --tag=$PKG_TAG
-fi
-echo "   done"
-popd > /dev/null
 
 # ########## temma-skel-api ##########
 # check if the directory exists
 if [ ! -d /opt/temma-skel-api ]; then
 	echo "No directory '/opt/temma-skel-api' found."
-	exit 4
+else
+	echo "== Update 'temma-skel-api'"
+	# copy files
+	pushd /opt/Temma > /dev/null
+	cp    bin/comma              /opt/temma-skel-api/bin/
+	cp    etc/apache.conf        /opt/temma-skel-api/etc/
+	cp    etc/temma-mini-api.php /opt/temma-skel-api/etc/temma.php
+	cp    etc/temma-full.php     /opt/temma-skel-api/etc/
+	cp -a etc/asynk              /opt/temma-skel-api/etc/
+	cp    tests/*                /opt/temma-skel-api/tests/
+	cp    www/index.php          /opt/temma-skel-web/www/
+	cp    www/.htaccess          /opt/temma-skel-api/www/
+	cp    .htaccess              /opt/temma-skel-api/
+	# manage composer.json
+	pushd /opt/temma-skel-api > /dev/null
+	sed "s/\"digicreon\/temma-lib\": \"[^\"]*\"/\"digicreon\/temma-lib\": \"$PKG_TAG\"/" composer.json > composer.json.tmp
+	mv composer.json.tmp composer.json
+	# commit tiles
+	if [[ -n $(git status --porcelain) ]]; then
+		git add .
+		git commit -m "Update from Temma main repository version '$PKG_TAG'."
+		git push
+		dpk pkg --tag=$PKG_TAG
+	fi
+	echo "   done"
+	popd > /dev/null
 fi
-echo "== Update 'temma-skel-api'"
-# copy files
-pushd /opt/Temma > /dev/null
-cp    bin/comma              /opt/temma-skel-api/bin/
-cp    etc/apache.conf        /opt/temma-skel-api/etc/
-cp    etc/temma-mini-api.php /opt/temma-skel-api/etc/temma.php
-cp    etc/temma-full.php     /opt/temma-skel-api/etc/
-cp -a etc/asynk              /opt/temma-skel-api/etc/
-cp    tests/*                /opt/temma-skel-api/tests/
-cp    www/index.php          /opt/temma-skel-web/www/
-cp    www/.htaccess          /opt/temma-skel-api/www/
-cp    .htaccess              /opt/temma-skel-api/
-# commit tiles
-pushd /opt/temma-skel-api > /dev/null
-if [[ -n $(git status --porcelain) ]]; then
-	git add .
-	git commit -m "Update from Temma main repository version '$PKG_TAG'."
-	git push
-	dpk pkg --tag=$PKG_TAG
-fi
-echo "   done"
-popd > /dev/null
 
